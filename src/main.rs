5b001227b94dd5631b115a447af325f764a72b3d
sjahl <636687+sjahl@users.noreply.github.com>
2025-08-31 13:38:19 -0400

    accept a cli flag for a date to fetch scores, handle empty list

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,40 @@
 // TODO:
-// 3. accept a command line argument with a date
+// 4. instead of relying on ordering, check for home/away status for each competitor.
+use clap::Parser;
+
 use serde::Deserialize;
 
-#[derive(Deserialize)]
+// command line
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    // Date to fetch scores for
+    #[arg(short, long)]
+    date: Option<String>,
+
+    // League to fetch scores for
+    #[arg(short, long, default_value = "eng.1")]
+    league: String,
+}
+
+// API Response data structures
+#[derive(Deserialize, Debug)]
 struct Team {
     abbreviation: String,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 struct Competitor {
     score: String,
     team: Team,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 struct Competitions {
     competitors: Vec<Competitor>,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 struct Event {
     // might use these fields later?
@@ -30,25 +46,41 @@ struct Event {
     competitions: Vec<Competitions>,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 struct EventsList {
     events: Vec<Event>,
 }
 
-fn fetch_events() -> Result<EventsList, Box<dyn std::error::Error>> {
-    let events = reqwest::blocking::get(
-        "http://site.api.espn.com/apis/site/v2/sports/soccer/eng.2/scoreboard",
-    )?
-    .json::<EventsList>()?;
+fn fetch_events(
+    date_str: Option<String>,
+    league: String,
+) -> Result<EventsList, Box<dyn std::error::Error>> {
+    let date_str = match date_str {
+        Some(date_str) => format!("?dates={}", date_str),
+        None => "".to_string(),
+    };
+    let req_url = format!(
+        "http://site.api.espn.com/apis/site/v2/sports/soccer/{}/scoreboard{}",
+        league, date_str
+    );
+    let events = reqwest::blocking::get(req_url)?.json::<EventsList>()?;
 
     Ok(events)
 }
 
 fn main() {
     println!("Welcome to the score checker!");
-    println!("Checking the scores for the EFL championship");
 
-    let todays_games = match fetch_events() {
+    let args = Args::parse();
+
+    println!("You selected the league: {}", args.league);
+
+    let maybe_date = match args.date {
+        Some(date) => Some(date),
+        None => None,
+    };
+
+    let todays_games = match fetch_events(maybe_date, args.league) {
         Ok(games) => games,
         Err(e) => {
             println!("Failed to fetch events: {e}");
@@ -56,13 +88,17 @@ fn main() {
         }
     };
 
-    for event in &todays_games.events {
-        println!(
-            "{} {} - {} {}",
-            event.competitions[0].competitors[0].team.abbreviation,
-            event.competitions[0].competitors[0].score,
-            event.competitions[0].competitors[1].score,
-            event.competitions[0].competitors[1].team.abbreviation,
-        );
+    if todays_games.events.len() > 0 {
+        for event in &todays_games.events {
+            println!(
+                "{} {} - {} {}",
+                event.competitions[0].competitors[0].team.abbreviation,
+                event.competitions[0].competitors[0].score,
+                event.competitions[0].competitors[1].score,
+                event.competitions[0].competitors[1].team.abbreviation,
+            );
+        }
+    } else {
+        println!("No games scheduled");
     }
 }
